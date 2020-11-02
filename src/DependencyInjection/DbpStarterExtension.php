8740c887405a51a70c6f79ccbb0d5104e61908a0
Christoph Reiter <reiter.christoph@gmail.com>
2020-11-02 11:25:41 +0100

    Add a simple entity & data provider

--- src/DependencyInjection/DbpStarterExtension.php
+++ src/DependencyInjection/DbpStarterExtension.php
@@ -13,10 +13,23 @@ class DbpStarterExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
+        $this->extendArrayParameter(
+            $container, 'api_platform.resource_class_directories', [__DIR__.'/../Entity']);
+
         $loader = new YamlFileLoader(
             $container,
             new FileLocator(__DIR__.'/../Resources/config')
         );
         $loader->load('services.yaml');
+    }
+
+    private function extendArrayParameter(ContainerBuilder $container, string $parameter, array $values)
+    {
+        if (!$container->hasParameter($parameter)) {
+            $container->setParameter($parameter, []);
+        }
+        $oldValues = $container->getParameter($parameter);
+        assert(is_array($oldValues));
+        $container->setParameter($parameter, array_merge($oldValues, $values));
     }
 }
---
+++ src/Entity/Place.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DBP\API\StarterBundle\Entity;
+
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ApiResource(
+ *     collectionOperations={"get"},
+ *     itemOperations={"get"},
+ *     iri="https://schema.org/Place",
+ *     normalizationContext={"groups"={"Place:output"}, "jsonld_embed_context"=true}
+ * )
+ */
+class Place
+{
+    /**
+     * @ApiProperty(identifier=true)
+     */
+    private $identifier;
+
+    /**
+     * @ApiProperty(iri="https://schema.org/name")
+     * @Groups({"Place:output"})
+     *
+     * @var string
+     */
+    private $name;
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getIdentifier(): string
+    {
+        return $this->identifier;
+    }
+
+    public function setIdentifier(string $identifier): void
+    {
+        $this->identifier = $identifier;
+    }
+}
